4e392221aff4286fa2c0be6d42ae47c71aedde6d
Lizaveta <lizaveta@Lizavetas-MacBook-Air.local>
2026-08-25 09:19:21 +0200

    norm errors fixed

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ldubok <ldubok@student.42warsaw.pl>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/23 14:09:05 by ldubok            #+#    #+#             */
-/*   Updated: 2026/08/23 15:01:16 by ldubok           ###   ########.fr       */
+/*   Updated: 2026/08/25 09:18:49 by ldubok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,41 @@ enum e_mode
 	SIMPLE, // 0
 	MEDIUM, // 1
 	COMPLEX, // 2
-	ADAPTIVE,  // 3
-}
+	ADAPTIVE, // 3
+};
 
 enum e_ops
 {
-	SA, SB, SS, PA, PB, RA, RB, RR, RRA, RRB, RRR
-}
+	SA,
+	SB,
+	SS,
+	PA,
+	PB,
+	RA,
+	RB,
+	RR,
+	RRA,
+	RRB,
+	RRR
+};
 
 typedef struct s_benchmark
 {
-	enum e_mode mode = ADAPTIVE;
-	size_t *ops_counters = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	enum e_mode	mode;
+	size_t		*ops_counters;
 	// benchmark mode
-	int benchmark_on = 0;
-} t_benchmark;
+	int			benchmark_on;
+}	t_benchmark;
 
 typedef struct s_node
 {
 	int				*content;
+	struct s_node	*prev;
 	struct s_node	*next;
-} t_node;
+}	t_node;
 
 typedef struct s_stack
 {
 	t_node	*head;
 	t_node	*tail;
-}
+}	t_stack;
