e946202fe1953ba479b141730083c6bb4cd6abfa
Mark <markp94@msn.com>
2026-09-02 21:43:27 +0100

    Simple sort(selection) and sort for three numbers

--- sort_three.c
+++ sort_three.c
@@ -3,14 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   sort_three.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldubok <ldubok@student.42warsaw.pl>        +#+  +:+       +#+        */
+/*   By: mapearso <mapearso@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/02 17:57:01 by mapearso          #+#    #+#             */
-/*   Updated: 2026/09/02 22:33:41 by ldubok           ###   ########.fr       */
+/*   Created: 2026/09/02 17:57:07 by mapearso          #+#    #+#             */
+/*   Updated: 2026/09/02 22:41:35 by mapearso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+/*
+Sort three numbers
+If top is biggest then move it to the back
+else if mid is biggest, shift everything down, leaving mid as last
+If new top is bigger than mid, swap top and mid
+conditions 1 and 3 hit: 5 4 3  ->  4 3 5   ->   3 4 5
+condition 3 hit: 4 3 5  ->  3 4 5
+condition 2 hit: 4 5 3  ->  3 4 5
+*/
 
 void	sort_three(t_environment *env)
 {
@@ -18,17 +28,17 @@ void	sort_three(t_environment *env)
 	int	mid;
 	int	bot;
 
-	if (env->stack_a.length != 3)
+	if (env->stack_a->length != 3)
 		return ;
 
-	top = env->stack_a.head->value;
-	mid = env->stack_a.head->next->value;
-	bot = env->stack_a.head->next->next->value;
+	top = env->stack_a->head->value;
+	mid = env->stack_a->head->next->value;
+	bot = env->stack_a->head->next->next->value;
 
 	if (top >= mid && top >= bot)
 		ft_ra_op(env);
 	else if (mid >= top && mid >= bot)
 		ft_rra_op(env);
-	if (env->stack_a.head->value > mid = env->stack_a.head->next->value)
+	if (env->stack_a->head->value > mid)
 		ft_sa_op(env);
 }
