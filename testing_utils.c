0a70afe982b7aa468301b40a30b0b5dd00a65f0d
Lizaveta <lizaveta@Lizavetas-MacBook-Air.local>
2026-09-02 22:53:25 +0200

    ranking almost done

--- testing_utils.c
+++ testing_utils.c
@@ -5,25 +5,52 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldubok <ldubok@student.42warsaw.pl>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/02 20:48:05 by ldubok            #+#    #+#             */
-/*   Updated: 2026/09/02 21:30:03 by ldubok           ###   ########.fr       */
+/*   Created: 2026/08/30 20:31:50 by ldubok            #+#    #+#             */
+/*   Updated: 2026/09/02 22:52:56 by ldubok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_environment	*get_test_env(int *array)
+t_environment	*ft_env_initialisation()
 {
-	int				i;
 	t_environment	*env;
 
-	i = 0;
-	env = ft_env_initialisation();
-	while (array[i])
-	{
-		ft_push_stack(env->stack_a, array[i], -1);
-		i++;
-	}
+	env = malloc(sizeof(t_environment));
+	if(!env)
+		return (NULL);
+	env->mode = ADAPTIVE;
+	env->ops_counters = malloc(sizeof(size_t) * 11);
+	env->benchmark_on = 0;
+	env->stack_a = malloc(sizeof(t_stack));
+	env->stack_b = malloc(sizeof(t_stack));
+	if(!(env->ops_counters && env->stack_a && env->stack_b))
+		return (0);
+	env->stack_a->length = 0;
+	env->stack_b->length = 0;
 	return (env);
 }
 
+void	ft_assign_ranks(t_environment *env)
+{
+	int	i;
+	t_node	*min_non_ranked;
+	t_node	*current_node;
+
+	i = 0;
+	min_non_ranked = env->stack_a->head;
+	while (i < env->stack_a->length)
+	{
+		while (min_non_ranked->rank >= 0)
+			min_non_ranked = min_non_ranked->next;
+		current_node = env->stack_a->head;
+		while (current_node->next != env->stack_a->head)
+		{
+			if (current_node->rank < 0 && current_node->value < min_non_ranked->value)
+				min_non_ranked = current_node;
+			current_node = current_node->next;
+		}
+		min_non_ranked->rank = i;
+		i++;
+	}
+}
