19f783046b79dc409240ff5220d77a31a1250698
Lizaveta <lizaveta@Lizavetas-MacBook-Air.local>
2026-09-05 16:18:59 +0200

    rotate fix

--- rotate_op.c
+++ rotate_op.c
@@ -6,7 +6,7 @@
 /*   By: ldubok <ldubok@student.42warsaw.pl>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/31 17:35:25 by ldubok            #+#    #+#             */
-/*   Updated: 2026/08/31 17:39:47 by ldubok           ###   ########.fr       */
+/*   Updated: 2026/09/05 16:18:16 by ldubok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,11 @@ int	ft_rb_op(t_environment *env)
 
 int	ft_rr_op(t_environment *env)
 {
-	ft_ra_op(env);
-	ft_rb_op(env);
+	if(env->stack_a->length < 2)
+		return (1);
+	ft_rotate_stack(env->stack_a);
+	if(env->stack_b->length < 2)
+		return (1);
+	ft_rotate_stack(env->stack_b);
 	return (1);
 }
