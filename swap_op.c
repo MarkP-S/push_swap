2963a390e6fe32722dac3a9dde5a177b3accde77
Lizaveta <lizaveta@Lizavetas-MacBook-Air.local>
2026-09-07 18:18:23 +0200

    swap_op.c

--- swap_op.c
+++ swap_op.c
@@ -6,7 +6,7 @@
 /*   By: ldubok <ldubok@student.42warsaw.pl>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/31 17:30:55 by ldubok            #+#    #+#             */
-/*   Updated: 2026/09/07 17:46:15 by ldubok           ###   ########.fr       */
+/*   Updated: 2026/09/07 18:18:06 by ldubok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_sa_op(t_env *env)
 {
-	if(env->stack_a->length < 2)
+	if (env->stack_a->length < 2)
 		return (1);
 	ft_swap_stack(env->stack_a);
 	print_op(env, SA);
@@ -23,7 +23,7 @@ int	ft_sa_op(t_env *env)
 
 int	ft_sb_op(t_env *env)
 {
-	if(env->stack_b->length < 2)
+	if (env->stack_b->length < 2)
 		return (1);
 	ft_swap_stack(env->stack_b);
 	print_op(env, SB);
@@ -32,9 +32,9 @@ int	ft_sb_op(t_env *env)
 
 int	ft_ss_op(t_env *env)
 {
-	if(env->stack_a->length >= 2)
+	if (env->stack_a->length >= 2)
 		ft_swap_stack(env->stack_a);
-	if(env->stack_b->length >= 2)
+	if (env->stack_b->length >= 2)
 		ft_swap_stack(env->stack_b);
 	print_op(env, SS);
 	return (1);
